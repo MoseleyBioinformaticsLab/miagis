3c8573b6c0f3eb8d33b0d2f5a7e0ca4ec515be72
Travis Thompson <ptth222@uky.edu>
2022-07-08 12:52:49 -0400

    Trying to get automations to work.

--- src/miagis/__init__.py
+++ src/miagis/__init__.py
@@ -14,6 +14,9 @@ This package has the following modules:
 ``user_input_checking``
     This module contains functions to validate user input.
     
+``print_map_layers``
+    This module contains the code to run the print_map_layers command.
+    
 
 """
 __version__ = "1.0.0"
